cafdb3c46d999a68ad7f8fea56b70e731df86ec3
Caio Lucas <lienscarlet@gmail.com>
2020-07-04 16:30:14 -0300

    add routes

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,7 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+
+import { Container } from './styles';
+
+const SignIn: React.FC = () => (<Container />);
+export default SignIn;

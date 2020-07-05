1d3515fc03a98a90cb30274a1df1f5c97d80f6be
Caio Lucas <lienscarlet@gmail.com>
2020-07-04 21:44:34 -0300

    add button and input

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import { Container, Title } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
+
+import {
+  Container, Title, ForgotPassword, ForgotPasswordText,
+} from './styles';
 
 const SignIn: React.FC = () => (
   <Container>
     <Image source={logoImg} />
 
     <Title>Faça seu logon</Title>
+
+    <Input name="email" icon="mail" placeholder="E-mail" />
+    <Input name="email" icon="lock" placeholder="Senha" />
+    <Button>Entrar</Button>
+
+    <ForgotPassword><ForgotPasswordText>Esqueci minha senha</ForgotPasswordText></ForgotPassword>
   </Container>
 );
 export default SignIn;
